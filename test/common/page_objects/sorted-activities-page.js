a5e148f178e965f9c5a99919ea77199e533ba72d
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2016-12-14 17:27:30 +0000

    Hide activities for some categories
    - hide 3 categories on the bottom by default
    - add show/hide button

--- test/common/page_objects/sorted-activities-page.js
+++ test/common/page_objects/sorted-activities-page.js
@@ -9,6 +9,11 @@ const SortedActivitiesPage = function SortedActivitiesPage(browser) {
     browser.queryAll(`[data-test="category-${category}"] li span`).map(x => x.innerHTML.trim());
   this.getCategoryDescription = category =>
     browser.text(`[data-test="category-${category}"] p`);
+  this.isCategoryDescriptionVisible = category =>
+    !browser.query(`[data-test="category-${category}"] [data-test="details"]`)
+    .className.split(/\s+/).includes('js-hidden');
+  this.expandCategory = category =>
+    browser.click(`[data-test="category-${category}"] [data-test="details-button"]`);
 };
 
 module.exports = SortedActivitiesPage;
