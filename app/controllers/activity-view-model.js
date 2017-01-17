115655437f1e3937367652affaed35b3ca086d18
nathancashmore <nathancashmore@ucds.email>
2017-01-17 12:05:33 +0000

    Change structure of re-sort and sorted page to only display show/hide of certain categories if those categories have activities underneath them.

--- app/controllers/activity-view-model.js
+++ app/controllers/activity-view-model.js
@@ -6,11 +6,11 @@ module.exports = class ActivityViewModel {
   constructor(activities) {
     this.activities = sortActivities(activities.map(this.activityModel));
     this.perCategory = {
-      DOING: this.activities.filter(x => x.category === 'DOING'),
-      READY: this.activities.filter(x => x.category === 'READY'),
-      HELP: this.activities.filter(x => x.category === 'HELP'),
-      NOTWORKED: this.activities.filter(x => x.category === 'NOT-WORKED'),
-      NO: this.activities.filter(x => x.category === 'NO'),
+      DOING: this.activityStats(this.activities.filter(x => x.category === 'DOING')),
+      READY: this.activityStats(this.activities.filter(x => x.category === 'READY')),
+      HELP: this.activityStats(this.activities.filter(x => x.category === 'HELP')),
+      NOTWORKED: this.activityStats(this.activities.filter(x => x.category === 'NOT-WORKED')),
+      NO: this.activityStats(this.activities.filter(x => x.category === 'NO')),
     };
   }
 
@@ -22,5 +22,12 @@ module.exports = class ActivityViewModel {
         // eslint-disable-next-line no-underscore-dangle
         details: i18n.__(`activity.${activity.activity}.details`),
       }, activity);
+  }
+
+  activityStats(activites) {
+    return ({
+      activities: activites,
+      hasActivities: activites.length > 0,
+    });
   }
 };
