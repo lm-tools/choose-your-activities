236d1670c4b1fe3f2315bb16e1fa088e697ab9f8
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2017-02-08 15:36:29 +0000

    Move "english and math" activity to 10th position

--- test/integration/support/test-data.js
+++ test/integration/support/test-data.js
@@ -13,26 +13,26 @@ module.exports = {
     { name: 'ACT-7', title: 'Find out what makes a good job application' },
     { name: 'ACT-8', title: 'Write down what you learnt last time you applied for a job' },
     { name: 'ACT-9', title: "Ask for feedback on some jobs you've applied for" },
-    { name: 'ACT-10', title: "Update your CV for jobs you're interested in" },
-    { name: 'ACT-11', title: 'Get a friend to look at your CV for you' },
-    { name: 'ACT-12', title: 'Search online for jobs' },
+    { name: 'ACT-10', title: 'Do you need to do anything about the basics - maths or English?' },
+    { name: 'ACT-11', title: "Update your CV for jobs you're interested in" },
+    { name: 'ACT-12', title: 'Get a friend to look at your CV for you' },
+    { name: 'ACT-13', title: 'Search online for jobs' },
     {
-      name: 'ACT-13',
+      name: 'ACT-14',
       title: 'Find out how social media websites like LinkedIn can help with your search',
     },
-    { name: 'ACT-14', title: 'Sign up for a few more job search websites' },
-    { name: 'ACT-15', title: 'Plan your work search for the week' },
+    { name: 'ACT-15', title: 'Sign up for a few more job search websites' },
+    { name: 'ACT-16', title: 'Plan your work search for the week' },
     {
-      name: 'ACT-16',
+      name: 'ACT-17',
       title: 'Make a list of a few jobs you’d like in a year from now ' +
       'and the skills you need for them',
     },
     {
-      name: 'ACT-17',
+      name: 'ACT-18',
       title: 'Find a qualification that could improve your chances of getting jobs you want',
     },
-    { name: 'ACT-18', title: "Use a website to check what skills you've got" },
-    { name: 'ACT-19', title: 'Do you need to do anything about the basics - maths or English?' },
+    { name: 'ACT-19', title: "Use a website to check what skills you've got" },
   ],
 
   categories: [
