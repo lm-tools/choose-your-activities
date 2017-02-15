7329f9965e5f6daadbd5fbd1aafe560299ec1173
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2017-02-15 12:26:09 +0000

    Refactor sorted/resorted page objects
    Introduce class inheritance

--- test/common/page_objects/re-sort-activities-page.js
+++ test/common/page_objects/re-sort-activities-page.js
@@ -1,22 +1,17 @@
-const ReSortActivitiesPage = function ReSortActivitiesPage(browser) {
-  this.browser = browser;
+const SortedActivitiesBasePage = require('./sorted-activities-base-page');
+class ReSortActivitiesPage extends SortedActivitiesBasePage {
 
-  this.activityList = () => browser.queryAll('[data-test="title"]');
-  this.visit = (accountId) => this.browser.visit(`/${accountId}/activities/sorted/resort`);
-  this.browserPath = () => browser.location.pathname;
-  this.activityCategories = () => browser.queryAll('[data-test="category"]').map(x => x.innerHTML);
-  this.getActivitiesInCategory = (category) =>
-    browser.queryAll(`[data-test="category-${category}"] li p`).map(x => x.innerHTML.trim());
-  this.getCategoryDescription = category =>
-    browser.text(`[data-test="category-${category}"] [data-test="empty-message"]`);
-  this.isCategoryDescriptionVisible = category =>
-    !browser.query(`[data-test="category-${category}"] [data-test="details"]`)
-      .className.split(/\s+/).includes('js-hidden');
-  this.isMoveButtonDisplayed = (activity) =>
-    !!browser.query(`[data-test="activity-${activity.name}-move"]`);
-  this.clickMoveButton = (activity) =>
-    browser.click(`[data-test="activity-${activity.name}-move"]`);
-  this.clickFinishSortingButton = () => browser.click('[data-test="finish-sorting-button"]');
-};
+  visit(accountId) {
+    return this.browser.visit(`/${accountId}/activities/sorted/resort`);
+  }
+
+  clickMoveButton(activity) {
+    return this.browser.click(`[data-test="activity-${activity.name}-move"]`);
+  }
+
+  clickFinishSortingButton() {
+    return this.browser.click('[data-test="finish-sorting-button"]');
+  }
+}
 
 module.exports = ReSortActivitiesPage;
--- test/common/page_objects/sorted-activities-page.js
+++ test/common/page_objects/sorted-activities-page.js
@@ -1,23 +1,14 @@
-const SortedActivitiesPage = function SortedActivitiesPage(browser) {
-  this.browser = browser;
+const SortedActivitiesBasePage = require('./sorted-activities-base-page');
+class SortedActivitiesPage extends SortedActivitiesBasePage {
 
-  this.activityList = () => browser.queryAll('[data-test="title"]');
-  this.visit = (accountId) => this.browser.visit(`/${accountId}/activities/sorted`);
-  this.browserPath = () => browser.location.pathname;
-  this.activityCategories = () => browser.queryAll('[data-test="category"]').map(x => x.innerHTML);
-  this.getActivitiesInCategory = (category) =>
-    browser.queryAll(`[data-test="category-${category}"] li p`).map(x => x.innerHTML.trim());
-  this.getOpenCategory = (category) =>
-    browser.query(`[data-test="category-${category}"] details[open]`);
-  this.getCategoryDescription = category =>
-    browser.text(`[data-test="category-${category}"] [data-test="empty-message"]`);
-  this.clickDetailsButton = activity =>
-    browser.click(`[data-test="activity-${activity.name}"] [data-test="details-button"]`);
-  this.clickReSortActivitiesLink = () => browser.click('[data-test="re-sort-link"]');
-  this.getCategorySummary = (category) =>
-    browser.text(`[data-test="category-${category}"] [data-test="show-hide-text"]`);
-  this.isMoveButtonDisplayed = (activity) =>
-    !!browser.query(`[data-test="activity-${activity.name}-move"]`);
-};
+  visit(accountId) {
+    return this.browser.visit(`/${accountId}/activities/sorted`);
+  }
+
+  clickReSortActivitiesLink() {
+    return this.browser.click('[data-test="re-sort-link"]');
+  }
+
+}
 
 module.exports = SortedActivitiesPage;
