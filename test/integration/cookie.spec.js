abdd14f50f3cae6bf6bf04ccbc3addafc17551a0
williamlacy <williamlacy@ucds.email>
2017-07-06 09:33:41 +0100

    Add generic govuk cookies page applicable to lmt (#29)
    (cherry picked from commit 0a7e309 helloworld)
    
    (cherry picked from commit 6fe3aca ryws)

---
+++ test/integration/cookie.spec.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-underscore-dangle */
+const helper = require('./support/integrationSpecHelper');
+const googleTagManagerHelper = helper.googleTagManagerHelper;
+const cookiePage = helper.cookiePage;
+const expect = require('chai').expect;
+const uuid = require('uuid');
+
+describe('Cookie page', () => {
+  const accountId = uuid.v4();
+
+  before(() =>
+    cookiePage.visit(accountId)
+  );
+
+  it('should contain valid google tag manager data', () =>
+    expect(googleTagManagerHelper.getUserVariable()).to.equal(accountId)
+  );
+
+  it('displays govuk general cookie info', () =>
+    expect(cookiePage.isDisplayed()).to.equal(true)
+  );
+
+  describe('Without account id', () => {
+    before(() =>
+      cookiePage.visit()
+    );
+
+    it('displays govuk general cookie info', () => {
+      expect(cookiePage.isDisplayed()).to.equal(true);
+    });
+  });
+});
