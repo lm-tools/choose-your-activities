91188551a68f3c2fd929a0746248d2d69b357de0
aneezsidhik <aneezsidhik@digital.uc.dwp.gov.uk>
2018-05-17 17:04:22 +0100

    @OR-1184: Grouping of categories for different prototypes

--- app/controllers/activity-group-mapping.js
+++ app/controllers/activity-group-mapping.js
@@ -1,3 +1,5 @@
+const defaultMapping = require('../models/activities');
+
 const commonMapping = {
   'GRP-1': [
     'ACT-7',
@@ -57,35 +59,15 @@ const commonMapping = {
   ],
 };
 
-const defaultMapping = [
-  'ACT-1',
-  'ACT-2',
-  'ACT-3',
-  'ACT-4',
-  'ACT-5',
-  'ACT-6',
-  'ACT-7',
-  'ACT-8',
-  'ACT-9',
-  'ACT-10',
-  'ACT-11',
-  'ACT-12',
-  'ACT-13',
-  'ACT-14',
-  'ACT-15',
-  'ACT-16',
-  'ACT-17',
-  'ACT-18',
-  'ACT-19',
-];
 const config = {
   a: commonMapping,
   b: commonMapping,
 };
 
-function getActivitiesForGroup(version, group) {
+function getActivitiesForGroup(version, group, excludeActivities=[]) {
   const versionMapping = config[version];
-  return versionMapping && versionMapping[group] || defaultMapping;
+  return (versionMapping && versionMapping[group] || defaultMapping)
+    .filter(val => !excludeActivities.includes(val));
 }
 
 function getCountOfActivitiesForGroup(version, group) {
