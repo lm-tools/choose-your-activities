79e2f24d34592673e6df5f13a4e72df0bded813f
aneezsidhik <aneezsidhik@digital.uc.dwp.gov.uk>
2018-06-01 10:49:10 +0100

    @OR-1185: after code review comments

--- app/controllers/activities-controller.js
+++ app/controllers/activities-controller.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = new express.Router({ mergeParams: true });
-const i18n = require('i18n');
 
 const ActivitiesModel = require('../models/activity-model');
 const ActivitiesViewModel = require('./unsorted-activity-view-model');
@@ -12,17 +11,10 @@ router.get('/', (req, res) => {
 
   ActivitiesModel.findUnsortedByVersionAccountIdAndGroup(version, accountId, group)
     .then(activities => {
-      // eslint-disable-next-line no-underscore-dangle
-      const title = i18n.__(`activity-group.${group}.title`);
-      // eslint-disable-next-line no-underscore-dangle
-      const canDo = i18n.__(activities.length > 1 ? 'activities.can-do.other'
-        : 'activities.can-do.one', activities.length);
       res.render('activities', Object.assign({
         accountId,
-        title,
-        canDo,
         group,
-      }, new ActivitiesViewModel(activities, false)));
+      }, new ActivitiesViewModel(activities, false, '', group)));
     });
 });
 
--- app/controllers/unsorted-activity-view-model.js
+++ app/controllers/unsorted-activity-view-model.js
@@ -1,13 +1,21 @@
 const decorateActivity = require('../locales/activity-decorator');
 const sortActivities = require('./activity-sorter');
+const i18n = require('i18n');
 
 module.exports = class ActivityViewModel {
 
-  constructor(unsorted, toSort, lastSortedActivity = '') {
+  constructor(unsorted, toSort, lastSortedActivity, group = '') {
     this.activities = unsorted.map(decorateActivity);
     this.activities = toSort ? sortActivities(this.activities) : this.activities;
     if (lastSortedActivity) {
       this.categorisedActivity = decorateActivity(lastSortedActivity);
+    }
+    if (group) {
+      // eslint-disable-next-line no-underscore-dangle
+      this.title = i18n.__(`activity-group.${group}.title`);
+      // eslint-disable-next-line no-underscore-dangle
+      this.canDo = i18n.__(this.activities.length > 1 ? 'activities.can-do.other'
+        : 'activities.can-do.one', this.activities.length);
     }
   }
 };
