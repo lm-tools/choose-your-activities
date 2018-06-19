1d46d8a31494ee61cc8329be4463088b0e6685cd
mangogrin <37105143+mangogrin@users.noreply.github.com>
2018-06-19 16:03:13 +0100

    @OR-1207: update delivery unit config for prototype go live

--- config/du-ver-mapping.js
+++ config/du-ver-mapping.js
@@ -6,9 +6,9 @@ module.exports = {
       'c', // default
     ],
     default: 'c',
-    'e7cf5d5a-0617-4005-8535-16dd99f201ca': 'a',
     '76783bd7-f709-46bf-8ea4-b0379e7ae1d3': 'a',
-    'ddb5befe-402c-473e-bc53-08471bfd87c3': 'b',
+    'e7cf5d5a-0617-4005-8535-16dd99f201ca': 'a',
+    '3d14d406-4a4d-4e73-a3e8-0dbd590d0194': 'b',
     'dffa6471-cab3-4775-8e9a-af91236e02ba': 'b',
   },
   test: {
@@ -18,9 +18,9 @@ module.exports = {
       'c', // default
     ],
     default: 'c',
-    'e7cf5d5a-0617-4005-8535-16dd99f201ca': 'a',
     '76783bd7-f709-46bf-8ea4-b0379e7ae1d3': 'a',
-    'ddb5befe-402c-473e-bc53-08471bfd87c3': 'b',
+    'e7cf5d5a-0617-4005-8535-16dd99f201ca': 'a',
+    '3d14d406-4a4d-4e73-a3e8-0dbd590d0194': 'b',
     'dffa6471-cab3-4775-8e9a-af91236e02ba': 'b',
   },
   production: {
@@ -30,5 +30,9 @@ module.exports = {
       'c', // default
     ],
     default: 'c',
+    '76783bd7-f709-46bf-8ea4-b0379e7ae1d3': 'a',
+    'e7cf5d5a-0617-4005-8535-16dd99f201ca': 'a',
+    '3d14d406-4a4d-4e73-a3e8-0dbd590d0194': 'b',
+    'dffa6471-cab3-4775-8e9a-af91236e02ba': 'b',
   },
 };
